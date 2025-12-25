7deccbf1a57ba2087d12305edac579c565f9b03d
Azea <azea.avenbright@gmail.com>
2025-12-25 11:33:51 -0700

    Part 8.1

--- composeApp/src/commonMain/kotlin/org/menagerie/puppet_master/state_machine/editor/views/NodeView.kt
+++ composeApp/src/commonMain/kotlin/org/menagerie/puppet_master/state_machine/editor/views/NodeView.kt
@@ -1,9 +1,9 @@
 package org.menagerie.puppet_master.state_machine.editor.views
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,16 +14,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.onClick
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.PointerButton
-import androidx.compose.ui.input.pointer.PointerEvent
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.boundsInRoot
 import androidx.compose.ui.layout.onGloballyPositioned
@@ -36,7 +32,6 @@ import org.menagerie.puppet_master.state_machine.editor.NodeEditorViewModel
 import org.menagerie.puppet_master.toSerializableOffset
 import org.menagerie.puppet_master.toSize
 
-@OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
 @Composable
 fun NodeView(
     node: Node,
@@ -52,12 +47,12 @@ fun NodeView(
     Box(modifier = Modifier.width(widthInDp).height(heightInDp)) {
         Card(modifier = Modifier
             .padding(8.dp)
-            .onClick(
-                enabled = true,
-                matcher = { it.button == PointerButton.Secondary },
-                onClick = {},
-                onDoubleClick = { editorViewModel.deleteNode(node.id) }
-            )) {
+            .pointerInput(node.id) {
+                detectTapGestures(onLongPress = {
+                    editorViewModel.deleteNode(node.id)
+                })
+            }
+        ) {
             Column {
                 Box(
                     contentAlignment = Alignment.Center,
